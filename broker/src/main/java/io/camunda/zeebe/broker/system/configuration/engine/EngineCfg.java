69f1d1a3939e5179af5562390ed4c5fab4596f38
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2024-03-29 17:09:27 +0700

    feat(): Introduce an experimental limiting feature to the timers
    By default, the limit is `Long.MAX`

--- broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/EngineCfg.java
+++ broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/EngineCfg.java
@@ -10,16 +10,20 @@ package io.camunda.zeebe.broker.system.configuration.engine;
 import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
 import io.camunda.zeebe.broker.system.configuration.ConfigurationEntry;
 import io.camunda.zeebe.engine.EngineConfiguration;
+import io.camunda.zeebe.util.ExponentialBackoff;
 
 public final class EngineCfg implements ConfigurationEntry {
 
   private MessagesCfg messages = new MessagesCfg();
   private CachesCfg caches = new CachesCfg();
 
+  private TimersCfg timers = new TimersCfg();
+
   @Override
   public void init(final BrokerCfg globalConfig, final String brokerBase) {
     messages.init(globalConfig, brokerBase);
     caches.init(globalConfig, brokerBase);
+    timers.init(globalConfig, brokerBase);
   }
 
   public MessagesCfg getMessages() {
@@ -38,15 +42,37 @@ public final class EngineCfg implements ConfigurationEntry {
     this.caches = caches;
   }
 
+  public TimersCfg getTimers() {
+    return timers;
+  }
+
+  public void setTimers(final TimersCfg timers) {
+    this.timers = timers;
+  }
+
   @Override
   public String toString() {
-    return "EngineCfg{" + "messages=" + messages + ", caches=" + caches + '}';
+    return "EngineCfg{"
+        + "messages="
+        + messages
+        + ", caches="
+        + caches
+        + ", timers="
+        + timers
+        + '}';
   }
 
   public EngineConfiguration createEngineConfiguration() {
     return new EngineConfiguration()
         .setMessagesTtlCheckerBatchLimit(messages.getTtlCheckerBatchLimit())
         .setMessagesTtlCheckerInterval(messages.getTtlCheckerInterval())
-        .setDrgCacheCapacity(caches.getDrgCacheCapacity());
+        .setDrgCacheCapacity(caches.getDrgCacheCapacity())
+        .setTimerLimit(timers.getLimit())
+        .setTimerOverLimitBackoff(
+            new ExponentialBackoff(
+                timers.getBackoffMaxValue(),
+                timers.getBackoffMinValue(),
+                timers.getBackoffFactor(),
+                timers.getBackoffJitterFactor()));
   }
 }
--- engine/src/main/java/io/camunda/zeebe/engine/EngineConfiguration.java
+++ engine/src/main/java/io/camunda/zeebe/engine/EngineConfiguration.java
@@ -7,6 +7,7 @@
  */
 package io.camunda.zeebe.engine;
 
+import io.camunda.zeebe.util.ExponentialBackoff;
 import java.time.Duration;
 
 public final class EngineConfiguration {
@@ -22,9 +23,27 @@ public final class EngineConfiguration {
 
   public static final int DEFAULT_DRG_CACHE_CAPACITY = 1000;
 
+  public static final long DEFAULT_TIMER_LIMIT = Long.MAX_VALUE;
+  public static final long DEFAULT_TIMER_OVER_LIMIT_BACKOFF_MIN_VALUE =
+      Duration.ofSeconds(10).toMillis();
+  public static final long DEFAULT_TIMER_OVER_LIMIT_BACKOFF_MAX_VALUE =
+      Duration.ofSeconds(60).toMillis();
+  public static final double DEFAULT_TIMER_OVER_LIMIT_BACKOFF_FACTOR = 1.6;
+  public static final double DEFAULT_TIMER_OVER_LIMIT_JITTER_FACTOR = 0.1;
+
+  public static final ExponentialBackoff DEFAULT_TIMER_OVER_LIMIT_BACKOFF =
+      new ExponentialBackoff(
+          DEFAULT_TIMER_OVER_LIMIT_BACKOFF_MAX_VALUE,
+          DEFAULT_TIMER_OVER_LIMIT_BACKOFF_MIN_VALUE,
+          DEFAULT_TIMER_OVER_LIMIT_BACKOFF_FACTOR,
+          DEFAULT_TIMER_OVER_LIMIT_JITTER_FACTOR);
+
   private int messagesTtlCheckerBatchLimit = DEFAULT_MESSAGES_TTL_CHECKER_BATCH_LIMIT;
   private Duration messagesTtlCheckerInterval = DEFAULT_MESSAGES_TTL_CHECKER_INTERVAL;
   private int drgCacheCapacity = DEFAULT_DRG_CACHE_CAPACITY;
+
+  private long timerLimit = DEFAULT_TIMER_LIMIT;
+  private ExponentialBackoff timerOverLimitBackoff = DEFAULT_TIMER_OVER_LIMIT_BACKOFF;
 
   public int getMessagesTtlCheckerBatchLimit() {
     return messagesTtlCheckerBatchLimit;
@@ -52,6 +71,25 @@ public final class EngineConfiguration {
 
   public EngineConfiguration setDrgCacheCapacity(final int drgCacheCapacity) {
     this.drgCacheCapacity = drgCacheCapacity;
+    return this;
+  }
+
+  public long getTimerLimit() {
+    return timerLimit;
+  }
+
+  public EngineConfiguration setTimerLimit(final long timerLimit) {
+    this.timerLimit = timerLimit;
+    return this;
+  }
+
+  public ExponentialBackoff getTimerOverLimitBackoff() {
+    return timerOverLimitBackoff;
+  }
+
+  public EngineConfiguration setTimerOverLimitBackoff(
+      final ExponentialBackoff timerOverLimitBackoff) {
+    this.timerOverLimitBackoff = timerOverLimitBackoff;
     return this;
   }
 }
